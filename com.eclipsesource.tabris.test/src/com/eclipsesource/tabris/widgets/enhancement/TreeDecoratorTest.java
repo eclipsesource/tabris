0b5db13fa9ac7c5e5a268af85131163fcfed7c7d
Moritz Post <mpost@eclipsesource.com>
2013-01-08 16:00:18 +0100

    Added setBackButtonFocus to TreeDecorator.
    Change-Id: I871e0ae09e55f909a40a282790feb448b1e06af6

--- com.eclipsesource.tabris.test/src/com/eclipsesource/tabris/widgets/enhancement/TreeDecoratorTest.java
+++ com.eclipsesource.tabris.test/src/com/eclipsesource/tabris/widgets/enhancement/TreeDecoratorTest.java
@@ -11,10 +11,17 @@
 package com.eclipsesource.tabris.widgets.enhancement;
 
 import static com.eclipsesource.tabris.internal.WidgetsUtil.TABRIS_VARIANT;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
 
+import org.eclipse.rap.rwt.testfixture.Fixture;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Tree;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,17 +30,27 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import com.eclipsesource.tabris.widgets.enhancement.TreeDecorator.TreePart;
 
-
 @RunWith( MockitoJUnitRunner.class )
 public class TreeDecoratorTest {
+  
+  private static final String VARIANT_BACK_FOCUS = "BACK_FOCUS";
   
   @Mock
   private Tree tree;
   private TreeDecorator decorator;
+  private Display display;
+
   
   @Before
   public void setUp() {
+    Fixture.setUp();
+    display = new Display();
     decorator = Widgets.onTree( tree );
+  }
+  
+  @After
+  public void tearDown() {
+    Fixture.tearDown();
   }
   
   @Test
@@ -64,4 +81,32 @@ public class TreeDecoratorTest {
     verify( tree ).setData( eq( TABRIS_VARIANT ), eq( "ALT_SELECTION" ) );
   }
   
+  @Test
+  public void testSetBackButtonFocusShouldSetBackFocusVariant() {
+    Shell shell = new Shell( display );
+    Tree focusTree = new Tree( shell, SWT.NONE );
+    TreeDecorator treeDecorator = Widgets.onTree( focusTree );
+    
+    treeDecorator.enableBackButtonNavigation();
+        
+    assertEquals( VARIANT_BACK_FOCUS, focusTree.getData( TABRIS_VARIANT ) );
+  }
+  
+  @Test
+  public void testSetBackButtonFocusShouldSetNullVariantOnOtherTreesWithBackFocusVariant() {
+    Shell shell = new Shell( display );
+    Tree tree1 = new Tree( shell, SWT.NONE );
+    Tree tree2 = new Tree( shell, SWT.NONE );
+    Tree tree3 = new Tree( shell, SWT.NONE );
+    tree2.setData( TABRIS_VARIANT, VARIANT_BACK_FOCUS );
+    tree3.setData( TABRIS_VARIANT, "anyVariant" );
+    TreeDecorator treeDecorator = Widgets.onTree( tree1 );
+    
+    treeDecorator.enableBackButtonNavigation();
+    
+    assertEquals( VARIANT_BACK_FOCUS, tree1.getData( TABRIS_VARIANT ) );
+    assertNull( VARIANT_BACK_FOCUS, tree2.getData( TABRIS_VARIANT ) );
+    assertEquals( "anyVariant", tree3.getData( TABRIS_VARIANT ) );
+  }
+
 }
--- com.eclipsesource.tabris/src/com/eclipsesource/tabris/widgets/enhancement/TreeDecorator.java
+++ com.eclipsesource.tabris/src/com/eclipsesource/tabris/widgets/enhancement/TreeDecorator.java
@@ -1,36 +1,60 @@
 /*******************************************************************************
- * Copyright (c) 2012 EclipseSource and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    EclipseSource - initial API and implementation
+ * Copyright (c) 2012 EclipseSource and others. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html Contributors:
+ * EclipseSource - initial API and implementation
  ******************************************************************************/
 package com.eclipsesource.tabris.widgets.enhancement;
 
+import static com.eclipsesource.tabris.internal.WidgetsUtil.TABRIS_VARIANT;
 import static com.eclipsesource.tabris.internal.WidgetsUtil.setVariant;
 
+import org.eclipse.swt.internal.widgets.IDisplayAdapter;
+import org.eclipse.swt.internal.widgets.WidgetTreeVisitor;
+import org.eclipse.swt.internal.widgets.WidgetTreeVisitor.AllWidgetTreeVisitor;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Tree;
-
+import org.eclipse.swt.widgets.Widget;
 
 /**
  * @since 0.8
  */
+@SuppressWarnings("restriction")
 public class TreeDecorator extends WidgetDecorator<TreeDecorator> {
-  
+
+  private static final String VARIANT_BACK_FOCUS = "BACK_FOCUS";
+
+  private class BackButtonVariantTreeVisistor extends AllWidgetTreeVisitor {
+
+    @Override
+    public boolean doVisit( Widget widget ) {
+      if( widget instanceof Tree && widget != tree ) {
+        return removeVariantBackFocus( widget );
+      }
+      return true;
+    }
+
+    private boolean removeVariantBackFocus( Widget widget ) {
+      Object variant = widget.getData( TABRIS_VARIANT );
+      if (variant != null && variant.equals( VARIANT_BACK_FOCUS )) {
+        setVariant( widget, null );
+        return false;
+      }
+      return true;
+    }
+  }
+
   public enum TreePart {
     LEAF, BRANCH, ALL
   }
-  
   private final Tree tree;
 
   public TreeDecorator( Tree tree ) {
     super( tree );
     this.tree = tree;
   }
-  
+
   public TreeDecorator useTitle( String title ) {
     tree.setToolTipText( title );
     return this;
@@ -46,11 +70,28 @@ public class TreeDecorator extends WidgetDecorator<TreeDecorator> {
       break;
       case BRANCH:
         setVariant( tree, "ALT_SELECTION_BRANCH" );
-        break;
+      break;
       case ALL:
         setVariant( tree, "ALT_SELECTION" );
-        break;
+      break;
     }
     return this;
   }
+
+  /**
+   * @since 0.10
+   */
+  public void enableBackButtonNavigation() {
+    setVariant( tree, VARIANT_BACK_FOCUS );
+    setVariantToNullOnOtherTrees();
+  }
+
+  private void setVariantToNullOnOtherTrees() {
+    IDisplayAdapter displayAdapter = tree.getDisplay().getAdapter( IDisplayAdapter.class );
+    Composite[] shells = displayAdapter.getShells();
+    for( int i = 0; i < shells.length; i++ ) {
+      WidgetTreeVisitor.accept( shells[ i ], new BackButtonVariantTreeVisistor() );
+    }
+  }
+
 }
