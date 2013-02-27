29e7dfce630fdb6683a62b72aa20c2c4247e38a4
hstaudacher <hstaudacher@eclipsesource.com>
2013-02-27 11:02:11 +0100

    Adds ScrollingComposite.

---
+++ com.eclipsesource.tabris.test/src/com/eclipsesource/tabris/widgets/ScrollingCompositeTest.java
@@ -0,0 +1,209 @@
+/*******************************************************************************
+ * Copyright (c) 2013 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    EclipseSource - initial API and implementation
+ ******************************************************************************/
+package com.eclipsesource.tabris.widgets;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import org.eclipse.rap.rwt.lifecycle.PhaseId;
+import org.eclipse.rap.rwt.testfixture.Fixture;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class ScrollingCompositeTest {
+
+  private Shell shell;
+
+  @Before
+  public void setUp() {
+    Fixture.setUp();
+    Display display = new Display();
+    shell = new Shell( display );
+    Fixture.fakePhase( PhaseId.PROCESS_ACTION );
+  }
+
+  @After
+  public void tearDown() {
+    Fixture.tearDown();
+  }
+
+  @Test( expected = IllegalArgumentException.class )
+  public void testFailsWithNullParent() {
+    new ScrollingComposite( null, SWT.H_SCROLL );
+  }
+
+  @Test
+  public void testUsesScrolledCompositeAsParent() {
+    ScrollingComposite composite = new ScrollingComposite( shell, SWT.H_SCROLL );
+
+    Composite parent = composite.getParent();
+
+    assertTrue( parent instanceof ScrolledComposite );
+  }
+
+  @Test
+  public void testExpandsHorizontal() {
+    ScrollingComposite composite = new ScrollingComposite( shell, SWT.H_SCROLL );
+    ScrolledComposite scrolledComposite = composite.getScrolledComposite();
+
+    boolean expandHorizontal = scrolledComposite.getExpandHorizontal();
+
+    assertTrue( expandHorizontal );
+  }
+
+  @Test
+  public void testExpandsVertical() {
+    ScrollingComposite composite = new ScrollingComposite( shell, SWT.H_SCROLL );
+    ScrolledComposite scrolledComposite = composite.getScrolledComposite();
+
+    boolean expandVertical = scrolledComposite.getExpandVertical();
+
+    assertTrue( expandVertical );
+  }
+
+  @Test
+  public void testDelegatesLayoutData() {
+    ScrollingComposite composite = new ScrollingComposite( shell, SWT.H_SCROLL );
+    ScrolledComposite scrolledComposite = composite.getScrolledComposite();
+    GridData data = new GridData();
+
+    composite.setLayoutData( data );
+
+    assertSame( data, scrolledComposite.getLayoutData() );
+  }
+
+  @Test
+  public void testSetsMinsize() {
+    ScrollingComposite composite = new ScrollingComposite( shell, SWT.H_SCROLL );
+    ScrolledComposite scrolledComposite = composite.getScrolledComposite();
+    Point size = composite.computeSize( SWT.DEFAULT, SWT.DEFAULT );
+
+    int minWidth = scrolledComposite.getMinWidth();
+    int minHeight = scrolledComposite.getMinHeight();
+
+    assertEquals( size.x, minWidth );
+    assertEquals( size.y, minHeight );
+  }
+
+  @Test
+  public void testSetsMinsizeOnResize() {
+    ScrollingComposite composite = new ScrollingComposite( shell, SWT.H_SCROLL );
+    ScrolledComposite scrolledComposite = composite.getScrolledComposite();
+    scrolledComposite.setSize( 100, 100 );
+    Point size = composite.computeSize( SWT.DEFAULT, SWT.DEFAULT );
+
+    int minWidth = scrolledComposite.getMinWidth();
+    int minHeight = scrolledComposite.getMinHeight();
+
+    assertEquals( size.x, minWidth );
+    assertEquals( size.y, minHeight );
+  }
+
+  @Test
+  public void testRevealsItem() {
+    shell.setSize( 100, 100 );
+    shell.setLayout( new FillLayout() );
+    ScrollingComposite composite = new ScrollingComposite( shell, SWT.V_SCROLL );
+    composite.setLayout( new RowLayout( SWT.VERTICAL ) );
+    createChildren( composite );
+    Control child = composite.getChildren()[ 80 ];
+
+    composite.reveal( child );
+
+    assertTrue( composite.isRevealed( child ) );
+  }
+
+  @Test
+  public void testIsNotRevealedByDefault() {
+    shell.setSize( 100, 100 );
+    shell.setLayout( new FillLayout() );
+    ScrollingComposite composite = new ScrollingComposite( shell, SWT.V_SCROLL );
+    composite.setLayout( new RowLayout( SWT.VERTICAL ) );
+    createChildren( composite );
+    Control child = composite.getChildren()[ 80 ];
+    shell.layout( true, true );
+
+    boolean revealed = composite.isRevealed( child );
+
+    assertFalse( revealed );
+  }
+
+  private void createChildren( ScrollingComposite composite ) {
+    for( int i = 0; i < 100; i++ ) {
+      Label label = new Label( composite, SWT.NONE );
+      label.setText( "FooBar" );
+    }
+  }
+
+  @Test( expected = IllegalArgumentException.class )
+  public void testRevealFailsWithNullControl() {
+    ScrollingComposite composite = new ScrollingComposite( shell, SWT.V_SCROLL );
+
+    composite.reveal( null );
+  }
+
+  @Test( expected = IllegalArgumentException.class )
+  public void testIsRevealFailsWithNullControl() {
+    ScrollingComposite composite = new ScrollingComposite( shell, SWT.V_SCROLL );
+
+    composite.isRevealed( null );
+  }
+
+  @Test( expected = IllegalArgumentException.class )
+  public void testRevealFailsWithNonChild() {
+    ScrollingComposite composite = new ScrollingComposite( shell, SWT.V_SCROLL );
+    Composite control = new Composite( shell, SWT.NONE );
+
+    composite.reveal( control );
+  }
+
+  @Test( expected = IllegalArgumentException.class )
+  public void testIsRevealFailsWithNonChild() {
+    ScrollingComposite composite = new ScrollingComposite( shell, SWT.V_SCROLL );
+    Composite control = new Composite( shell, SWT.NONE );
+
+    composite.isRevealed( control );
+  }
+
+  @Test
+  public void testDisposesScrolledComposite() {
+    ScrollingComposite composite = new ScrollingComposite( shell, SWT.V_SCROLL );
+    ScrolledComposite scrolledComposite = composite.getScrolledComposite();
+
+    composite.dispose();
+
+    assertTrue( composite.isDisposed() );
+    assertTrue( scrolledComposite.isDisposed() );
+  }
+
+  @Test
+  public void testDoubleDisposingDoesNotFail() {
+    ScrollingComposite composite = new ScrollingComposite( shell, SWT.V_SCROLL );
+
+    composite.dispose();
+    composite.dispose();
+  }
+}
---
+++ com.eclipsesource.tabris/src/com/eclipsesource/tabris/widgets/ScrollingComposite.java
@@ -0,0 +1,205 @@
+/*******************************************************************************
+ * Copyright (c) 2013 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    EclipseSource - initial API and implementation
+ ******************************************************************************/
+package com.eclipsesource.tabris.widgets;
+
+import static com.eclipsesource.tabris.internal.Preconditions.checkArgumentNotNull;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.events.ControlAdapter;
+import org.eclipse.swt.events.ControlEvent;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+
+
+/**
+ * <p>
+ * A {@link ScrollingComposite} can be used the same way as a simple {@link Composite}. The difference between
+ * a {@link Composite} and a {@link ScrollingComposite} is that a {@link ScrollingComposite} shows scrollbars
+ * automatically when it's content becomes to big.
+ * </p>
+ * <p>
+ * You may noticed that there is a {@link ScrolledComposite}. A {@link ScrolledComposite} is not an easy widget. In
+ * most cases it's enough to have just a scrollable container for some content that grows dynamically.
+ * This task can not be accomplished easily with the {@link ScrolledComposite}. Thus the {@link ScrollingComposite}
+ * was created to make just the described task easy and nothing more.
+ * </p>
+ *
+ * <p>
+ * <b>Styles:</b> H_SCROLL, V_SCROLL
+ * </p>
+ *
+ * @see Composite
+ * @see ScrolledComposite
+ *
+ * @since 1.0
+ */
+public class ScrollingComposite extends Composite {
+
+  private ScrolledComposite scrolledComposite;
+
+  /**
+   * <p>
+   * Constructs a new {@link ScrollingComposite}. See {@link Composite#Composite(Composite, int)} for a more detailed
+   * description.
+   * </p>
+   *
+   * @see Composite#Composite(Composite, int)
+   */
+  public ScrollingComposite( Composite parent, int style ) {
+    super( createScrolledComposite( parent, checkStyle( style ) ), checkStyle( style ) );
+    scrolledComposite = ( ScrolledComposite )this.getParent();
+    initializeScrolledComposite();
+  }
+
+  private static int checkStyle( int style ) {
+    int mask = SWT.H_SCROLL | SWT.V_SCROLL | SWT.BORDER | SWT.LEFT_TO_RIGHT;
+    return style & mask;
+  }
+
+  private static ScrolledComposite createScrolledComposite( Composite parent, int style ) {
+    ScrolledComposite scrolledComposite = new ScrolledComposite( parent, style ) {
+      @Override
+      public void layout( boolean changed, boolean all ) {
+        super.layout( changed, all );
+        Control content = getContent();
+        if( content != null && !content.isDisposed() ) {
+          ( ( ScrollingComposite )content ).layout( changed, all );
+        }
+      }
+    };
+    scrolledComposite.setExpandVertical( true );
+    scrolledComposite.setExpandHorizontal( true );
+    return scrolledComposite;
+  }
+
+  private void initializeScrolledComposite() {
+    scrolledComposite.setContent( this );
+    handleScrollbars();
+    scrolledComposite.addControlListener( new ControlAdapter() {
+      @Override
+      public void controlResized( ControlEvent event ) {
+        handleScrollbars();
+      }
+    } );
+  }
+
+  @Override
+  public void layout( boolean changed, boolean all ) {
+    handleScrollbars();
+    super.layout( changed, all );
+  }
+
+  private void handleScrollbars() {
+    computeSize( SWT.DEFAULT, SWT.DEFAULT );
+  }
+
+  @Override
+  public Point computeSize( int wHint, int hHint, boolean changed ) {
+    Point size = super.computeSize( wHint, hHint, changed );
+    scrolledComposite.setMinSize( size );
+    return size;
+  }
+
+  @Override
+  public void setLayoutData( Object layoutData ) {
+    checkWidget();
+    scrolledComposite.setLayoutData( layoutData );
+  }
+
+  /**
+   * <p>
+   * Scrolls to the defined control until it's visible.
+   * </p>
+   *
+   * @param control the control to scroll to. Must not be <code>null</code>.
+   *
+   * @exception IllegalArgumentException when the defined control is <code>null</code> or not a children of
+   *                                     this Composite.
+   */
+  public void reveal( Control control ) {
+    checkRevealState( control );
+    scrolledComposite.showControl( control );
+  }
+
+  /**
+   * <p>
+   * Checks if a control is visible within the visible area. The control needs to be completely visible for a
+   * <code>true</code> result.
+   * </p>
+   *
+   * @param control the control to check the visibility. Must not be <code>null</code>.
+   *
+   * @exception IllegalArgumentException when the defined control is <code>null</code> or not a children of
+   *                                     this Composite.
+   */
+  public boolean isRevealed( Control control ) {
+    checkRevealState( control );
+    Point origin = scrolledComposite.getOrigin();
+    Rectangle clientArea = scrolledComposite.getClientArea();
+    Rectangle controlBounds = control.getBounds();
+    boolean verticalVisible = ( clientArea.height + origin.y - controlBounds.y - controlBounds.height ) >= 0
+                              && clientArea.height + origin.y <= controlBounds.y + clientArea.height;
+    boolean horizontalVisible = ( clientArea.width + origin.x - control.getLocation().x - controlBounds.width ) >= 0
+                                && ( clientArea.width + origin.x ) <= ( control.getLocation().x  + clientArea.width );
+    return horizontalVisible && verticalVisible;
+  }
+
+  private void checkRevealState( Control control ) {
+    checkArgumentNotNull( control, "Child to Reveal." );
+    checkContains( control );
+    checkWidget();
+  }
+
+  /**
+   * <p>
+   * Returns the underlying {@link ScrolledComposite} for advanced use cases.
+   * </p>
+   */
+  public ScrolledComposite getScrolledComposite() {
+    checkWidget();
+    return ( ScrolledComposite )getParent();
+  }
+
+  private void checkContains( Control control ) {
+    boolean contains = containsControl( control );
+    if( !contains ) {
+      throw new IllegalArgumentException( "Control is not a child." );
+    }
+  }
+
+  private boolean containsControl( Control control ) {
+    boolean contains = false;
+    if( control != null && !control.isDisposed() ) {
+      Composite parent = control.getParent();
+      while( parent != null && !( parent instanceof Shell ) && !contains ) {
+        if( this == parent ) {
+          contains = true;
+        }
+        parent = parent.getParent();
+      }
+    }
+    return contains;
+  }
+
+  @Override
+  public void dispose() {
+    if( scrolledComposite != null ) {
+      scrolledComposite = null;
+      getParent().dispose();
+    }
+    super.dispose();
+  }
+
+}
