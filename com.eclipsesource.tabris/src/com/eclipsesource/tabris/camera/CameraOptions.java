3a05ee972ef2c33a59eadd3df99f4be47d8f78ca
Moritz Post <mpost@eclipsesource.com>
2013-03-07 15:43:56 +0100

    Updated javadocs.
    Change-Id: I501d7c27ee70b3b01a3349d48683c009980ba378

--- com.eclipsesource.tabris/src/com/eclipsesource/tabris/camera/CameraOptions.java
+++ com.eclipsesource.tabris/src/com/eclipsesource/tabris/camera/CameraOptions.java
@@ -1,40 +1,35 @@
 /*******************************************************************************
- * Copyright (c) 2012 EclipseSource and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    EclipseSource - initial API and implementation
+ * Copyright (c) 2012 EclipseSource and others. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html Contributors:
+ * EclipseSource - initial API and implementation
  ******************************************************************************/
 package com.eclipsesource.tabris.camera;
 
 import org.eclipse.swt.graphics.Point;
 
-
 /**
  * <p>
- * The <code>CameraOptions</code> is used to configure a <code>Camera</code> object. Typically it's passed as a
- * constructor argument to a <code>Camera</code>
+ * The <code>CameraOptions</code> is used to configure a <code>Camera</code>
+ * object when taking a picture via {@link Camera#takePicture(CameraOptions)}.
  * </p>
- *
+ * 
  * @see Camera
  * @since 0.8
  */
 public class CameraOptions {
 
   public static final CameraOptions NONE = new CameraOptions();
-
   private Point resolution;
   private boolean saveToAlbum;
 
   /**
    * <p>
-   * Defines the preferred resolution for images. When possible images will have the configured size when sending them
-   * to the server.
+   * Defines the preferred resolution for images. When possible images will have
+   * the configured size when sending them to the server.
    * </p>
-   *
+   * 
    * @param x Preferred width. Must be positive.
    * @param y Preferred height. Must be positive.
    */
@@ -53,7 +48,7 @@ public class CameraOptions {
 
   /**
    * <p>
-   * When set the taken pictures willb e saved to the client's photo album.
+   * When set the taken pictures will be saved to the client's photo album.
    * </p>
    */
   public void setSaveToAlbum( boolean saveToAlbum ) {
@@ -62,11 +57,11 @@ public class CameraOptions {
 
   /**
    * <p>
-   * Returns if the taken pictures should be saved to the client's photo album or not.
+   * Returns if the taken pictures should be saved to the client's photo album
+   * or not.
    * </p>
    */
   public boolean savesToAlbum() {
     return saveToAlbum;
   }
-
 }
