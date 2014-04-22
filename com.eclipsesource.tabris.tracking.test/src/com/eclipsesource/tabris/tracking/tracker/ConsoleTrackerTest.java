e104a0ae23a46a39daca3e011f8c268cfdbaf952
hstaudacher <hstaudacher@eclipsesource.com>
2014-04-22 16:42:59 +0200

    Adds ConsoleTracker to tracking API
    Implements a Tracker for the Tracking API that uses System.out to print
    UI events. This little helper can help during development time to
    identify the page flow. It also acts as an example implementation if
    someone wants to build it's own Tracker e.g. to log UI navigation.
    
    Change-Id: I1de2a97338303b818879e7f77522e013beb81282

---
+++ com.eclipsesource.tabris.tracking.test/src/com/eclipsesource/tabris/tracking/tracker/ConsoleTrackerTest.java
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * Copyright (c) 2014 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    EclipseSource - initial API and implementation
+ ******************************************************************************/
+package com.eclipsesource.tabris.tracking.tracker;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.PrintStream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.eclipsesource.tabris.tracking.TrackingEvent;
+import com.eclipsesource.tabris.tracking.TrackingEvent.EventType;
+import com.eclipsesource.tabris.tracking.TrackingInfo;
+import com.eclipsesource.tabris.ui.ActionConfiguration;
+import com.eclipsesource.tabris.ui.PageConfiguration;
+
+
+public class ConsoleTrackerTest {
+
+  private PrintStream out;
+  private ConsoleTracker tracker;
+
+  @Before
+  public void setUp() {
+    out = mock( PrintStream.class );
+    System.setOut( out );
+    tracker = new ConsoleTracker();
+  }
+
+  @Test
+  public void testPrintsPageView() {
+    PageConfiguration config = mock( PageConfiguration.class );
+    when( config.getId() ).thenReturn( "foo" );
+    when( config.getTitle() ).thenReturn( "title" );
+    TrackingEvent event = new TrackingEvent( EventType.PAGE_VIEW, createInfo(), config, 23 );
+
+    tracker.handleEvent( event );
+
+    verify( out ).println( "Thu Jan 01 01:00:00 CET 1970: PAGE_VIEW - foo (title) [appId, model, vendor, osVersion]" );
+  }
+
+  @Test
+  public void testPrintsAction() {
+    ActionConfiguration config = mock( ActionConfiguration.class );
+    when( config.getId() ).thenReturn( "foo" );
+    when( config.getTitle() ).thenReturn( "title" );
+    TrackingEvent event = new TrackingEvent( EventType.ACTION, createInfo(), config, 23 );
+
+    tracker.handleEvent( event );
+
+    verify( out ).println( "Thu Jan 01 01:00:00 CET 1970: ACTION - foo (title) [appId, model, vendor, osVersion]" );
+  }
+
+  @Test
+  public void testPrintsSearch() {
+    ActionConfiguration config = mock( ActionConfiguration.class );
+    when( config.getId() ).thenReturn( "foo" );
+    when( config.getTitle() ).thenReturn( "title" );
+    TrackingEvent event = new TrackingEvent( EventType.SEARCH, createInfo(), config, 23 );
+
+    tracker.handleEvent( event );
+
+    verify( out ).println( "Thu Jan 01 01:00:00 CET 1970: SEARCH - query=query [appId, model, vendor, osVersion]" );
+  }
+
+  private TrackingInfo createInfo() {
+    TrackingInfo info = new TrackingInfo();
+    info.setAppId( "appId" );
+    info.setDeviceOsVersion( "osVersion" );
+    info.setDeviceVendor( "vendor" );
+    info.setDeviceModel( "model" );
+    info.setSearchQuery( "query" );
+    return info;
+  }
+}
