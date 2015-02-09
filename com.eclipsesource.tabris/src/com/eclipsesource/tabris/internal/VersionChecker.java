1d528894ae33b3991f75be7675d4b982ca65355e
hstaudacher <hstaudacher@eclipsesource.com>
2015-02-09 11:40:53 +0100

    Version Bump to 1.4.4
    Change-Id: I8720dcdc4dc005a11f3a057fe5387ba6fc13cf4b

--- com.eclipsesource.tabris/src/com/eclipsesource/tabris/internal/VersionChecker.java
+++ com.eclipsesource.tabris/src/com/eclipsesource/tabris/internal/VersionChecker.java
@@ -22,7 +22,7 @@ import com.eclipsesource.tabris.VersionCheck;
 
 public class VersionChecker {
 
-  static final String TABRIS_SERVER_VERSION = "1.4.3";
+  static final String TABRIS_SERVER_VERSION = "1.4.4";
   private static final Pattern VERSION_PATTERN = Pattern.compile( ".*/([0-9]*\\.[0-9]*\\.[0-9])* \\(.*\\)" );
 
   private final String clientVersion;
